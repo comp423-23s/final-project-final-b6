c4ace4204751f6c4a6f04b83f22ea3a31911419a
Jackson Davis <89363700+Jackson-Davis1@users.noreply.github.com>
2023-04-19 14:43:32 -0400

    List members on members tab (#80)

--- frontend/src/app/organization-details/members/members.component.ts
+++ frontend/src/app/organization-details/members/members.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { Observable } from 'rxjs';
+import {MemberService, User} from './members.service';
+import { PermissionService } from 'src/app/permission.service';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-members',
@@ -6,5 +12,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent {
+  public members$: Observable<User[]>;
+  public adminPermission$: Observable<boolean>;
 
+
+  constructor(
+    private memberService: MemberService,
+    private route: ActivatedRoute,
+    private permission: PermissionService,
+    protected deleteDialog: MatDialog,){
+      this.adminPermission$ = this.permission.check('admin.view', 'admin/')
+      const routeParams = this.route.snapshot.paramMap;
+      this.members$ = memberService.getMembers(String(routeParams.get('organizationName')));
+    }
 }
