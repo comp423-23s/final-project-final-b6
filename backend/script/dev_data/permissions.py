a3a804344003dc51a53a0bb059ac9563b8bfeb76
Antonio Tudela <97488232+antud@users.noreply.github.com>
2023-04-27 11:49:45 -0400

    Add manager permissions (#102)

--- backend/script/dev_data/permissions.py
+++ backend/script/dev_data/permissions.py
@@ -3,7 +3,7 @@
 from ...models import Permission
 from . import roles
 
-__authors__ = ["Kris Jordan"]
+__authors__ = ["Kris Jordan", "Antonio Tudela"]
 __copyright__ = "Copyright 2023"
 __license__ = "MIT"
 
@@ -11,8 +11,12 @@ pairs = [
     (roles.sudoer, Permission(action="*", resource="*")),
     (roles.staff, Permission(action="admin.*", resource="*")),
     (roles.staff, Permission(action="user.*", resource="*")),
-    (roles.staff, Permission(action="role.*", resource="*")),
+    (roles.staff, Permission(action="role.*", resource="*")),#event. and organization. 
     (roles.staff, Permission(action="checkin.*", resource="*")),
+    #the two additions below allow managers to do everything associted with events/ organizations
+    #if they should have some restrictions like not being able to create/delete, I can fix that in a PR
+    (roles.staff, Permission(action="organization.*", resource="*")),
+    (roles.staff, Permission(action="event.*", resource="*")),
     (roles.ambassador, Permission(action="user.search", resource="*")),
     (roles.ambassador, Permission(action="checkin.*", resource="*"))
 ]
